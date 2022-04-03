088adc61e3f8b6c6a7234f3da29dde23121ca1f7
Buuuuli <km509@duke.edu>
2022-04-03 17:51:15 -0400

    2

--- app.py
+++ app.py
@@ -37,7 +37,25 @@ app.layout = html.Div([
     ]),
     html.Br(),
     html.Div([
-
+    dcc.Upload(
+            id='image',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files')
+            ]),
+            style={
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+            # Allow multiple files to be uploaded
+            multiple=True
+        )
     ]),
     html.Br(),
     html.Button('Check', id='check-button', n_clicks=0),
--- scratch.py
+++ scratch.py
@@ -1,9 +1,28 @@
 from Call_Function import *
+import torch
+import os
+
+#model_path = 'Model'
+
+#filename = 'deep_fullmodel_new.pt'
+
+model2 = torch.load('Model/deep_fullmodel_new.pt')
+
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 age_input = 55
 sex_input = 'male'
 localization_input = 'trunk'
 
+image = 'webelement/ISIC_0024315.jpg'
+
+
 a = meta_pipeline(age_input,sex_input,localization_input)
 
 
+b = image_pipeline(image)
+
+
+j,k = test_model()
