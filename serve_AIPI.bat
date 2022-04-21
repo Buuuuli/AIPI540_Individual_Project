8868594ee28105ae14aa3c2344e077d9171eaca3
buuuuli <km509@duke.edu>
2022-04-21 17:12:56 -0400

    vcm

---
+++ serve_AIPI.bat
@@ -0,0 +1,4 @@
+cd C:\Users\vcm\PycharmProjects\AIPI540_Individual_Project
+git pull https://%AIPI_GIT_PAT%@github.com/Buuuuli/AIPI540_Individual_Project.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
